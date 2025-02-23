cd475a68e148ff2e2615fe0c0b0c7ec5b038a60e
Emil Sharkov <emosharkov@gmail.com>
2025-02-22 17:38:53 -0800

    added logging

--- api/src/config/settings.rs
+++ api/src/config/settings.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use tracing::info;
+
 use crate::config;
 
 #[derive(Debug, Clone)]
@@ -57,6 +59,7 @@ impl Settings {
     }
 
     fn get_database_settings() -> Result<DatabaseSettings, Box<dyn Error>> {
+        info!("Getting database settings");
         let database_settings = DatabaseSettings {
             host: std::env::var("DB_HOST")?,
             port: std::env::var("DB_PORT")?.parse()?,
@@ -69,6 +72,7 @@ impl Settings {
     }
     
     fn get_aws_settings() -> Result<AwsSettings, Box<dyn Error>> {
+        info!("Getting AWS settings");
         let aws_settings = AwsSettings {
             access_key_id: std::env::var("AWS_ACCESS_KEY_ID")?,
             secret_access_key: std::env::var("AWS_SECRET_ACCESS_KEY")?,
@@ -79,6 +83,7 @@ impl Settings {
     }
 
     pub fn get_middleware_settings() -> Result<MiddlewareSettings, Box<dyn Error>> {
+        info!("Getting middleware settings");
         let middleware_settings = MiddlewareSettings {
             timeout_seconds: std::env::var("TIMEOUT_SECONDS")?.parse()?,
             request_limit_per_hour: std::env::var("REQUEST_LIMIT_PER_HOUR")?.parse()?,
@@ -87,6 +92,7 @@ impl Settings {
     }
 
     pub fn get_redis_settings() -> Result<RedisSettings, Box<dyn Error>> {
+        info!("Getting Redis settings");
         let redis_settings = RedisSettings {
             host: std::env::var("REDIS_HOST")?,
             port: std::env::var("REDIS_PORT")?.parse()?,
--- db/README.md
+++ db/README.md
@@ -5,6 +5,10 @@
 ### Locally: 
 ```psql -h localhost -U postgres -d postgres -p 5432```
 
+## Backup DB
+1. ```pg_dump -h terraform-20241206002335676200000001.cru2wuokeaez.us-east-1.rds.amazonaws.com -U postgres postgres > backup_file.sql```
+2. ```gzip backup_file.sql``` 
+
 ## Create Tables
 Type ```\i path/init_db.sql``` using forward slashes for the absolute path
 
