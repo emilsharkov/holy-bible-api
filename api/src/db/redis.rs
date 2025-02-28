95de7a604b84c2005b4496c84b5180fd65379e93
Emil Sharkov <emosharkov@gmail.com>
2025-02-28 14:41:02 -0800

    edit readme

--- api/src/db/redis.rs
+++ api/src/db/redis.rs
@@ -1,7 +1,6 @@
 use redis::Client;
 use crate::config::settings::RedisSettings;
 use std::error::Error;
-use tracing::info;
 
 pub async fn get_client(settings: &RedisSettings) -> Result<Client, Box<dyn Error>> {
     let redis_url = format!(
@@ -9,6 +8,5 @@ pub async fn get_client(settings: &RedisSettings) -> Result<Client, Box<dyn Erro
         settings.host, settings.port
     );
     let client = Client::open(redis_url)?;
-    info!("Connected to redis");
     Ok(client)
 }
