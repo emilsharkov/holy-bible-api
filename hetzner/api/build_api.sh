283dc379dc2565e0335e345fb4c4cc6028d648f8
Emil Sharkov <emosharkov@gmail.com>
2025-07-01 23:58:37 -0700

    donezo

--- hetzner/api/build_api.sh
+++
@@ -1,8 +0,0 @@
-cd ~/holy-bible-api/api
-git checkout master
-git pull
-cargo build --release
-cp target/release/api /usr/local/bin/api
-sudo systemctl restart api
-sudo systemctl status api
-journalctl -u api -f
