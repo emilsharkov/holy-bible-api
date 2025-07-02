6bb96ba6c6328483d454eb4e12afb5bc8812b25a
Emil Sharkov <emosharkov@gmail.com>
2025-07-02 13:05:20 -0700

    beszel

---
+++ hetzner/beszel/init_beszel.sh
@@ -0,0 +1,7 @@
+curl -sL https://get.beszel.dev/hub -o /tmp/install-hub.sh && chmod +x /tmp/install-hub.sh && /tmp/install-hub.sh
+# login to localhost:8090
+# click create a system 
+# click on ssh key and use it for next command
+curl -sL https://get.beszel.dev -o /tmp/install-agent.sh && chmod +x /tmp/install-agent.sh && /tmp/install-agent.sh
+# go and create a slack notification
+# acces beszel @ localhost:8090
