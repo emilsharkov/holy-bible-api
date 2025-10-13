12033518261495dec3f2741723b1ddb1bdef9bb2
Emil Sharkov <emosharkov@gmail.com>
2025-10-12 22:52:53 -0700

    add log

--- api/src/main.rs
+++ api/src/main.rs
@@ -14,5 +14,6 @@ async fn main() {
     let settings = config::settings::Settings::new().unwrap();
     let router = app::router::get_app_router(&settings).await.unwrap();
     let listener = app::listener::bind_listener(&settings).await.unwrap();
+    tracing::info!("Server started on {}:{}", settings.server_settings.host, settings.server_settings.port);
     axum::serve(listener, router).await.unwrap();
 }
